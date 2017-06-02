47253b9745d1b2d729862a7ee3eb760c815a4231
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-02 14:21:13 +0300

    Added constants and BettedDoctorService class

---
+++ app/src/main/java/com/example/kiragu/finddoctor/BetterDoctorService.java
@@ -0,0 +1,8 @@
+package com.example.kiragu.finddoctor;
+
+/**
+ * Created by kiragu on 6/2/17.
+ */
+
+public class BetterDoctorService {
+}
--- app/src/main/java/com/example/kiragu/finddoctor/MainActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.kiragu.finddoctor;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.headerTextView) TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface amita = Typeface.createFromAsset(getAssets(), "fonts/Amita-Regular.ttf");
+        mTextView.setTypeface(amita);
+
     }
 }
