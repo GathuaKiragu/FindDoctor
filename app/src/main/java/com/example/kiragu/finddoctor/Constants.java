e57ddea49f8afe75c24ac8ead78f9b75f889ecc0
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-02 14:58:54 +0300

    Onclick listener on the findDoctor

--- app/src/main/java/com/example/kiragu/finddoctor/Constants.java
+++ app/src/main/java/com/example/kiragu/finddoctor/Constants.java
@@ -3,7 +3,7 @@ package com.example.kiragu.finddoctor;
 /**
  * Created by kiragu on 6/2/17.
  */
-
+//Initialize String Constants
 public class Constants {
     public static final String BETTER_DOCTOR_API = BuildConfig.BETTER_DOCTOR_API;
     public static final String API_BASE_URL = "https://api.betterdoctor.com/2016-03-01/doctors?location=";
--- app/src/main/java/com/example/kiragu/finddoctor/MainActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.kiragu.finddoctor;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -10,15 +13,26 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.headerTextView) TextView mTextView;
+    @Bind(R.id.findDoctorButton)
+    Button mFindDoctorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+//Font
         Typeface amita = Typeface.createFromAsset(getAssets(), "fonts/Amita-Regular.ttf");
         mTextView.setTypeface(amita);
+
+//Onclick listener to take user to the DoctorsActivity
+        mFindDoctorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DoctorsActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
