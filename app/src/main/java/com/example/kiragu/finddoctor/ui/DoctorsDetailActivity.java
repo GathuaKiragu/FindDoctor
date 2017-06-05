f7dfc91d2cb9ba91ba86a00b2af0f4949bb573cf
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-05 16:30:44 +0300

    Added a empty Activity

---
+++ app/src/main/java/com/example/kiragu/finddoctor/ui/DoctorsDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.kiragu.finddoctor.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kiragu.finddoctor.R;
+
+public class DoctorsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctors_detail);
+    }
+}
