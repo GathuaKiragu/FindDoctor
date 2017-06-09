35f974443cb5d0b4a01bc831b81711d6441b27bd
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-09 18:59:17 +0300

    interlinked the signup activity with the login activity

--- app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
@@ -1,15 +1,34 @@
 package com.example.kiragu.finddoctor.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
+import com.example.kiragu.finddoctor.DoctorsListActivity;
+import com.example.kiragu.finddoctor.MainActivity;
 import com.example.kiragu.finddoctor.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginTextView)
+    TextView mLoginTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mLoginTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
