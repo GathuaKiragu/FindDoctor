5c4f6f83cce4c5fd8f29a7dee3829fd31f0faa99
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-09 20:08:32 +0300

    Authenticated user with firebase

--- app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
@@ -18,7 +18,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
         mLoginTextView.setOnClickListener(this);
     }
--- app/src/main/java/com/example/kiragu/finddoctor/ui/SignUpActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/ui/SignUpActivity.java
@@ -1,15 +1,79 @@
 package com.example.kiragu.finddoctor.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kiragu.finddoctor.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+
+    @Bind(R.id.signUpButton)
+    Button mSignButton;
+    @Bind(R.id.userName)
+    EditText mUserName;
+    @Bind(R.id.emailEditText)
+    EditText mEmailEditText;
+    @Bind(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText)
+    EditText mConfirmPasswordEditText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mSignButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
-}
+    //Onclick listeners
+    @Override
+    public void onClick(View view) {
+
+        if (view == mSignButton) {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mSignButton) {
+            createNewUser();
+        }
+    }
+    private void createNewUser() {
+        final String username = mUserName.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+         }
+    }
