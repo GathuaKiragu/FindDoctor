cd18a27fec3d5d054905ebb4927bc3f2b0aaf020
Gathua Kiragu <kiragugathua@gmail.com>
2017-06-10 12:07:12 +0300

    Binded login views

--- app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
+++ app/src/main/java/com/example/kiragu/finddoctor/ui/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.example.kiragu.finddoctor.DoctorsListActivity;
 import com.example.kiragu.finddoctor.MainActivity;
@@ -14,6 +16,13 @@ import butterknife.ButterKnife;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.loginTextView)
     TextView mLoginTextView;
+    @Bind(R.id.loginButton)
+    Button mLoginButton;
+    @Bind(R.id.emailEditText)
+    EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
